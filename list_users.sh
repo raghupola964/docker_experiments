ffdfd104a6122e32d249a1ca223b59fbb3daf5b5
raghupola964 <126030021+raghupola964@users.noreply.github.com>
2024-04-05 12:56:38 -0400

    Create list_users.sh

---
+++ list_users.sh
@@ -0,0 +1,34 @@
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+#function helper {
+#       $no_of_args = 2
+#       if[$# -ne $no_of_args ];
+#       then
+#               echo "please execute scripts with correct args"
+#               echo "asd"
+#       fi
+#}
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
